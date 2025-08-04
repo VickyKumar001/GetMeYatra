b7bdedac5f30203a89b41a31ed93d5ce4a4e7830
VickyKumar001 <vk7434075@gmail.com>
2025-08-04 17:22:17 +0530

    adding new page

--- GetMeYartra/settings.py
+++ GetMeYartra/settings.py
@@ -74,17 +74,23 @@ WSGI_APPLICATION = 'GetMeYartra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'getmeyatradb',
+#         'USER': 'root',
+#         'PASSWORD': 'Vicky@$80510',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'getmeyatradb',
-        'USER': 'root',
-        'PASSWORD': 'Vicky@$80510',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
